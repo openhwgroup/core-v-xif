1e4308be1db9a1d7a24f496c71dad1146aff9cce
davide schiavone <davide@openhwgroup.org>
2024-01-17 15:21:36 +0100

    fix license

--- src/core_v_xif.sv
+++ src/core_v_xif.sv
@@ -2,6 +2,35 @@
 // Solderpad Hardware License, Version 2.1, see LICENSE.md for details.
 // SPDX-License-Identifier: Apache-2.0 WITH SHL-2.1
 
+/*
+ Copyright 2021 TU Wien
+
+ This file, and derivatives thereof are licensed under the
+ Solderpad License, Version 2.0 (the "License");
+ Use of this file means you agree to the terms and conditions
+ of the license and are in full compliance with the License.
+ You may obtain a copy of the License at
+
+ https://solderpad.org/licenses/SHL-2.0/
+
+ Unless required by applicable law or agreed to in writing, software
+ and hardware implementations thereof
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, EITHER EXPRESSED OR IMPLIED.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+// Copyright 2021 ETH Zurich and University of Bologna.
+// Copyright and related rights are licensed under the Solderpad Hardware
+// License, Version 0.51 (the "License"); you may not use this file except in
+// compliance with the License.  You may obtain a copy of the License at
+// http://solderpad.org/licenses/SHL-0.51. Unless required by applicable law
+// or agreed to in writing, software, hardware and materials distributed under
+// this License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the
+// specific language governing permissions and limitations under the License.
+
 // CORE-V-XIF Package
 // Contributor: Moritz Imfeld <moimfeld@student.ethz.ch>
 // Contributor: Michael Platzer <michael.platzer@hotmail.com>
