c67dbca7260f9b844c201f2a4e49fee1f9947128
davide schiavone <davide@openhwgroup.org>
2024-01-17 15:10:13 +0100

    removed MXLEN

--- src/core_v_xif.sv
+++ src/core_v_xif.sv
@@ -20,8 +20,7 @@ interface core_v_xif
   parameter int unsigned X_DUALREAD             = 0,  // Is dual read supported? 0: No, 1: Yes, for ``rs1``, 2: Yes, for ``rs1`` - ``rs2``, 3: Yes, for ``rs1`` - ``rs3``
   parameter int unsigned X_DUALWRITE            = 0,  // Is dual write supported? 0: No, 1: Yes.
   parameter int unsigned X_ISSUE_REGISTER_SPLIT = 0,  // Does the interface pipeline register interface? 0: No, 1: Yes.
-  parameter int unsigned X_MEM_WIDTH            = 32, // Memory access width for loads/stores via the eXtension interface
-  parameter int unsigned MXLEN                  = 32
+  parameter int unsigned X_MEM_WIDTH            = 32  // Memory access width for loads/stores via the eXtension interface
 );
 
   typedef logic [X_NUM_RS+X_DUALREAD-1:0] readregflags_t;
