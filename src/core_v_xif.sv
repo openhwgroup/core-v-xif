3d4c5c5887dbe29bfffa90862e12ee411d744dae
davide schiavone <davide@openhwgroup.org>
2024-02-01 15:36:38 +0100

    Add license 2.0

--- src/core_v_xif.sv
+++ src/core_v_xif.sv
@@ -1,42 +1,6 @@
 /*
  Copyright 2024 OpenhW Group
-
- This file, and derivatives thereof are licensed under the
- Solderpad License, Version 2.0 (the "License");
- Use of this file means you agree to the terms and conditions
- of the license and are in full compliance with the License.
- You may obtain a copy of the License at
-
- https://solderpad.org/licenses/SHL-2.0/
-
- Unless required by applicable law or agreed to in writing, software
- and hardware implementations thereof
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, EITHER EXPRESSED OR IMPLIED.
- See the License for the specific language governing permissions and
- limitations under the License.
-*/
-
-/*
  Copyright 2021 TU Wien
-
- This file, and derivatives thereof are licensed under the
- Solderpad License, Version 2.0 (the "License");
- Use of this file means you agree to the terms and conditions
- of the license and are in full compliance with the License.
- You may obtain a copy of the License at
-
- https://solderpad.org/licenses/SHL-2.0/
-
- Unless required by applicable law or agreed to in writing, software
- and hardware implementations thereof
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, EITHER EXPRESSED OR IMPLIED.
- See the License for the specific language governing permissions and
- limitations under the License.
-*/
-
-/*
  Copyright 2021 ETH Zurich and University of Bologna
 
  This file, and derivatives thereof are licensed under the
