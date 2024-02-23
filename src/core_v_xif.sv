063dc7fb75fb6fcfffe79c250b17c4075cd494cf
Christian Herber <christian.herber@oss.nxp.com>
2024-02-23 09:54:03 +0100

    Improving X_MISA parameter - clarifying what a CPU is expected to do with this parameter - Adjusting the range of the paramter   - old range was 32:0   - range according to RV spec is MXLEN-1:0   - new range is 25:0, as this is where the Extensions field is

--- src/core_v_xif.sv
+++ src/core_v_xif.sv
@@ -32,7 +32,7 @@ interface core_v_xif
   parameter int unsigned X_RFW_WIDTH            = 32, // Register file write access width for the eXtension interface
   parameter int unsigned X_NUM_HARTS            = 1,  // Number of harts (hardware threads) associated with the interface
   parameter int unsigned X_HARTID_WIDTH         = 1,  // Width of ``hartid`` signals.
-  parameter logic [31:0] X_MISA                 = '0, // MISA extensions implemented on the eXtension interface
+  parameter logic [25:0] X_MISA                 = '0, // MISA extensions implemented on the eXtension interface
   parameter logic [ 1:0] X_ECS_XS               = '0, // Initial value for mstatus.XS
   parameter int unsigned X_DUALREAD             = 0,  // Is dual read supported? 0: No, 1: Yes, for ``rs1``, 2: Yes, for ``rs1`` - ``rs2``, 3: Yes, for ``rs1`` - ``rs3``
   parameter int unsigned X_DUALWRITE            = 0,  // Is dual write supported? 0: No, 1: Yes.
